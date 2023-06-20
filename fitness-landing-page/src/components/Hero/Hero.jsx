6a2218f3d36d6c71fca1d647fa4eb1624a214c20
Raphael Melo <raphaelteles52@gmail.com>
2023-06-20 08:32:45 -0300

    hero buttons

--- fitness-landing-page/src/components/Hero/Hero.jsx
+++ fitness-landing-page/src/components/Hero/Hero.jsx
@@ -8,8 +8,52 @@ const Hero = () => {
       <div className="left-h">
         <Header />
 
+        <div className="the-best">
+          <div></div>
+          <span>a melhor academia da sua cidade </span>
+        </div>
+
+        <div className="hero-text">
+          <div>
+            <span className='stroke-text'>Molde </span>
+            <span>Seu
+            </span>
+          </div>
+
+          <div>
+            <span>Corpo Ideal</span>
+
+          </div>
+          <div>
+            <span>Aqui, vamos ajudá-lo a moldar e construir o seu corpo ideal e aproveitar ao máximo a vida. </span>
+          </div>
+        </div>
+
+        <div className='count'>
+          <div>
+            <span>+140</span>
+            <span>personais experientes</span>
+          </div>
+          <div>
+            <span>+987</span>
+            <span>alunos matriculados</span>
+          </div>
+          <div>
+            <span>+50</span>
+            <span>treinos personalizados</span>
+          </div>
+
+          <div className="hero-buttons">
+            <buttons className="btn">Começar</buttons>
+            <buttons className="btn">Saiba Mais</buttons>
+          </div>
+        </div>
+
+
       </div>
-      <div className="right-h"></div>
+      <div className="right-h">lado direito
+
+      </div>
     </div>
   )
 }
